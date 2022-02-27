cc1109cbd0da74706c22e1ec4ab539e03f087e26
GryphNdor <griffin@ogura-family.com>
2022-02-27 05:40:30 -0500

    finished home

--- pages/home.js
+++ pages/home.js
@@ -1,22 +1,20 @@
-import { Grid, Button, useMediaQuery } from '@mui/material'
+import { Grid, Button } from '@mui/material'
 import React from 'react'
 import Image from 'next/image'
 import styles from '../styles/Index.module.css'
 import Link from 'next/link'
 
 export default function home() {
-  const small = useMediaQuery('(max-width:600px)')
-
   return (
     <div>
-      <Grid container alignItems="center" flexDirection={small ? "column" : "row"} justifyContent="space-evenly" style={{ backgroundColor: '#384270', color: 'white', height: '100vh' }}>
+      <Grid container alignItems="center" justifyContent="space-evenly" style={{ backgroundColor: '#384270', color: 'white', height: '100vh' }}>
         <Grid item>
-          <h1 style={{ fontSize: small ? '3em' : '6em', width: small ? 400 : 800 }}>
+          <h1 style={{ fontSize: '6em', width: 800 }}>
             Keeping your room safe and secure
           </h1>
         </Grid>
         <Grid item>
-          <Image width={500} height={500} src="/image 1.svg" />
+          <Image width={600} height={600} src="/image 1.svg" />
         </Grid>
       </Grid>
       {/* lolol it works bruh */}
@@ -26,6 +24,8 @@ export default function home() {
           justifyContent: 'space-around',
           alignItems: 'center'
         }}>
+          
+
           <div style={{ display: 'flex', flexDirection: 'column' }}>
             <h1 style={{ fontSize: '4em', width: 592, color: 'white', padding: 0, margin: 0 }}>
               Our Vision
@@ -37,9 +37,9 @@ export default function home() {
               and does not invade on other people’s privacy.
             </p>
           </div>
-          <div>
-            <Image src="/vision 1 1.jpg" className={styles.rounded} width={500} height={400} objectFit="cover" />
-          </div>
+              <Image src="/vision 1 1.jpg" className={styles.rounded} width={500} height={400} objectFit="cover" />
+        
+            
         </div>
       </section>
       <section className={styles.section2}>
@@ -48,20 +48,18 @@ export default function home() {
             Our product
           </h1>
           <h3 style={{ fontSize: '2em', width: 735, color: 'black', fontWeight: 300 }}>
-            The Door Defender is a lightweight, resilient box that will be attached to the back of the door
+            The Door Defender is a lighweight, resilient box that will be attached to the back of the door
             that will be attached using strong adhesive. The Door Defender will detect the door opening and will contact
             you using an app. The app is also allows the user to disarm and arm the Door Defender and will also shows the users
             on the app.
           </h3>
           <Button variant="contained">
-            <Link href="/doorguard">
+            <Link href="/">
               View Product
             </Link>
           </Button>
         </div>
-        <div>
-          <Image width={600} height={600} className={styles.rounded} src="/20220226_174333.jpg" />
-        </div>
+            <Image width={600} height={600} className={styles.rounded} src="/20220226_174333.jpg" />
       </section >
     </div >
   )

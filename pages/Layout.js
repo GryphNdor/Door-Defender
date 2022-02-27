5e055b218bc83e633765cc79a3d7400b8af228e2
GryphNdor <griffin@ogura-family.com>
2022-02-27 03:04:36 -0500

    swapped index and home, also updated index a bit

--- pages/Layout.js
+++ pages/Layout.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography, Container } from '@mui/material'
+import { AppBar, Toolbar, Typography, Avatar, Menu, MenuItem } from '@mui/material'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useUser } from '@auth0/nextjs-auth0'
+import { useRouter } from 'next/router'
 
 export default function Layout({ children }) {
+  const router = useRouter()
 
   const { user } = useUser();
-
+  const [anchorEl, setAnchorEl] = React.useState();
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleLogout = () => {
+    router.push('/api/auth/logout')
+  }
 
   return (
     <div>
@@ -15,7 +27,7 @@ export default function Layout({ children }) {
         <Toolbar>
 
           <Image width={50} height={50} src="/logo.svg" />
-          <Typography varient="h1" component="div" style={{ fontWeight: 700 }} sx={{ flexGrow: 1, mr: 2 }}>
+          <Typography variant="h5" component="div" style={{ fontWeight: 700 }} sx={{ flexGrow: 1, mr: 2 }}>
 
             Door Defender
           </Typography>
@@ -34,10 +46,23 @@ export default function Layout({ children }) {
           )}
           {user && (
             <>
-              <img src={user.picture} alt={user.name} />
-              <Link href="/api/auth/logout">
+              <Avatar alt={user.name} sx={{ mr: 2 }} src={user.picture} onClick={handleClick} />
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  'aria-labelledby': 'basic-button',
+                }}
+              >
+                <MenuItem onClick={handleClose}>Settings</MenuItem>
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+              </Menu>
+              {/* <Link href="/api/auth/logout">
                 <a>Logout</a>
-              </Link>
+              </Link> */}
             </>
           )}
         </Toolbar>
